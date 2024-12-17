026f435df2430b8eb88dcce30af075ddb4eb92ef
Nonchalant <afrontier829@gmail.com>
2024-12-18 04:53:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+```CSharp
+using ObservableWrapperGenerator;
+using R3;
+
+public partial class BeforeCode
+{
+    private readonly Subject<int> _number = new();
+    public Observable<int> number => _number.AsObservable();
+    
+    private readonly BehaviorSubject<string> _text = new("AAA");
+    public Observable<int> text => _text.AsObservable();
+}
+
+public partial class AfterCode
+{
+    [ObservableWrapper]
+    private readonly Subject<int> _number = new();
+    
+    [ObservableWrapper] 
+    private readonly BehaviorSubject<string> _text = new("AAA");
+}
+```
+
+### Reference
+
+https://qiita.com/amenone_games/items/762cbea245f95b212cfa
---
+++ Sandbox/ObservableWrapperSandbox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ObservableWrapperGenerator;
+using R3;
+
+namespace Sandbox
+{
+    public partial class SubjectClass
+    {
+        [ObservableWrapper] 
+        private readonly Subject<int> _a = new Subject<int>();
+        
+        [ObservableWrapper] 
+        private readonly Subject<string> _b = new Subject<string>();
+        
+        public void Test() {
+            Console.WriteLine(a);
+        }
+    }
+    
+    public partial class BehaviorSubjectClass
+    {
+        [ObservableWrapper] 
+        private readonly BehaviorSubject<int> _c = new BehaviorSubject<int>(0);
+        
+        public void Test() {
+            Console.WriteLine(c);
+        }
+    }
+}
